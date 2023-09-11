2860608527a114c24a3800ab32865036c0dcc4b2
Joykhaan <perfectkhan421@gmail.com>
2023-09-11 11:53:27 +0600

    change server

--- index.js
+++ index.js
@@ -1,33 +1,92 @@
+
+
+// new code from here
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+// middlware
 app.use(cors());
+app.use(express.json());
 
-const tutorials =require('./data/courses.json');
-const tutorial = require('./data/tutorial.json');
 
-app.get('/',(req,res)=>{
-    res.send('look mama tor heda')
-});
-app.get('/recipe-tutorials',(req,res)=>{
-    res.send(tutorials)
-});
-app.get('/tutorial',(req,res)=>{
-    res.send(tutorial)
-});
-app.get('/tutorial/:id',(req,res)=>{
-    const id = req.params.id;
-    const selectedTutorial = tutorial.find(tut =>tut._id === id);
-    res.send(selectedTutorial)
-});
-app.get('/sub-tutorial/:id',(req,res)=>{
-    const id = req.params.id;
-    const selectedSubTutorial = tutorial.filter(subtut =>subtut.id === id);
-    res.send(selectedSubTutorial)
+
+const uri = `mongodb+srv://CookWithMe:s2J3a7o1fQuAKO2U@cluster0.wfqwiph.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userInfo = client.db('CookWithMe').collection('userInfo');
+        const tutorials = client.db('CookWithMe').collection('Tutorials');
+        const tutorial = client.db('CookWithMe').collection('Tutorial');
+        
+
+
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const cursor = userInfo.find(query);
+            const user = await cursor.toArray();
+            res.send(user);
+        })
+
+        app.post('/users', async (req, res) => {
+            const userinfo = req.body;
+            const result = await userInfo.insertOne(userinfo);
+            res.send(result)
+        })
+        
+        app.get('/recipe-tutorials', async(req, res) => {
+            // res.send(tutorials)
+            const query = {}
+            const cursor = tutorials.find(query);
+            const tutorial = await cursor.toArray();
+            res.send(tutorial);
+        });
+        app.post('/recipe-tutorials', async (req, res) => {
+            const tutorials = req.body;
+            const result = await tutorials.inserMany(tutorials);
+            res.send(result)
+        })
+        app.get('/tutorial', async(req, res) => {
+            // res.send(tutorial)
+            const query = {}
+            const cursor = tutorial.find(query);
+            const tutorials = await cursor.toArray();
+            res.send(tutorials);
+        });
+        app.get('/tutorial/:id', async(req, res) => {
+            const id = req.params.id;
+            const query ={_id: (id)}
+            const cursor = tutorial.find(query);
+            const tutorialsID = await cursor.toArray();
+            console.log(tutorialsID)
+            res.send(tutorialsID);
+        });
+        app.get('/sub-tutorial/:id', async(req, res) => {
+            const id = req.params.id;
+            const query ={id: (id)};
+            const cursor = tutorial.find(query);
+            const selectedSubTutorial = await cursor.toArray();
+            res.send(selectedSubTutorial)
+        });
+
+        
+
+    }
+    finally {
+
+
+    }
+}
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('server running....')
 });
 
-app.listen(port,()=>{
-    console.log(`server is running mama ${port}`)
+app.listen(port, () => {
+    console.log(`server is running on ${port}`)
 })
